174c3e0acf648fd9eff3c25a2f455f345243e77b
alfredo.gil <alfredo.gil@oramon.nl>
2017-08-03 18:32:33 +0200

    Import Mysql files from a compress file

---
+++ databases/importMysql/importMysqlDBBunzip.sh
@@ -0,0 +1,8 @@
+#TAGS: #IMPORT_DATABASE_BZ2 #MYSQL #BUNZIP2
+#Description: Import a database from a bz2 file directly into mysql.
+
+#bunzip2 < database.sql.bz2 | mysql -u root -proot costes
+
+#Description: Import a database from a gzip file directly into mysql.
+
+#gunzip < db.gz | mysql -u root -proot dbname
