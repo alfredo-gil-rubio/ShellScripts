1a9a23ac6063fa0f677d04b555045b9328edc2e2
alfredo.gil <alfredo.gil@oramon.nl>
2017-08-04 12:26:07 +0200

    Import Sql in in txt to a database

---
+++ databases/importMysql/executeScirptMysql.sh
@@ -0,0 +1,4 @@
+#TAGS: #IMPORT__SQL_DATABASE_MYSQL #MYSQL #BUNZIP2
+#Description: Import a sql txt procedures into a mysql database directly.
+
+#mysql -u root -proot costes < {path}/SanitizeScript.sql
