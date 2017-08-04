4faa588e67202a6ea8f54e235deafbbaae97c431
alfredo.gil <alfredo.gil@oramon.nl>
2017-08-04 12:52:33 +0200

    Change in a script pattern

--- databases/MySQL/executeScriptMysqlIntoADB.sh
+++ databases/MySQL/executeScriptMysqlIntoADB.sh
@@ -1,6 +1,6 @@
 #TAGS: #IMPORT__SQL_DATABASE_MYSQL #MYSQL #BUNZIP2
 #Description: Import a sql txt procedures into a mysql database directly.
 
-#mysql -u {PASSWORD} -p{PASSWORD} {DBNAME} < {SQL_FILENAME}
+#mysql -u {USER} -p{PASSWORD} {DBNAME} < {SQL_FILENAME}
 
 #e.g: mysql -u root -proot alfredoDB < /home/alfredo/Documents/SanitizeScript.sql
